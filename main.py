e940780f0b9e6d4bb3414dd9e791c281e4f9c7a6
sultansakai <ASAaytzhanov@outlook.com>
2024-05-01 07:15:32 +0500

    v1.2 added docs

--- main.py
+++ main.py
@@ -10,13 +10,25 @@ if __name__ == "__main__":
     try:
         # Creating an order
         order = example_order
-        payment = example_payment
         make_order = ioka.create_order(order.amount, order.currency)
         print(f"Order created: {make_order}")
+
+        # Getting all orders
         all_orders = ioka.get_all_orders()
         print(f"All orders: {all_orders}")
+
+        # Getting an order by id
+        order_by_id = ioka.get_order_by_id(make_order["order"]["id"])
+        print(f"Order by id: {order_by_id}")
+
+        # Creating a payment
+        payment = example_payment
         make_payment = ioka.create_card_payment(make_order["order"]["id"], payment.pan, payment.exp, payment.holder,
                                                 payment.cvc)
         print(f"Payment created: {make_payment}")
+
+        # Getting a payment by id
+        payment_by_id = ioka.get_payment(make_order["order"]["id"], make_payment["id"])
+        print(f"Payment by id: {payment_by_id}")
     except ioka_api_error as e:
         print(e)
--- src/client.py
+++ src/client.py
@@ -7,6 +7,7 @@ class IokaAPI:
         self.base_url = base_url
         self.api_key = api_key
 
+    # func to make a request to the API
     def _make_request(self, method, endpoint, data=None) -> dict:
         headers = {'API-KEY': self.api_key}
         url = f'{self.base_url}{endpoint}'
@@ -17,20 +18,25 @@ class IokaAPI:
         except requests.exceptions.RequestException as e:
             raise ioka_api_error(e)
 
+    # func to create an order
     def create_order(self, amount, currency) -> dict:
         data = {"amount": amount, "currency": currency, "capture_method": "AUTO"}
         return self._make_request("POST", "/orders", data)
 
+    # func to create a card payment
     def create_card_payment(self, order_id, pan, exp, holder, cvc, save=True) -> dict:
         data = {"pan": pan, "exp": exp, "holder": holder, "cvc": cvc, "save": save}
         endpoint = f"/orders/{order_id}/payments/card"
         return self._make_request("POST", endpoint, data)
 
+    # func to get all orders
     def get_all_orders(self) -> dict:
         return self._make_request("GET", "/orders", None)
 
-    def get_order(self, order_id) -> dict:
+    # func to get an order by id
+    def get_order_by_id(self, order_id) -> dict:
         return self._make_request("GET", f"/orders/{order_id}", None)
 
+    # func to get a payment by id
     def get_payment(self, order_id, payment_id) -> dict:
         return self._make_request("GET", f"/orders/{order_id}/payments/{payment_id}", None)
